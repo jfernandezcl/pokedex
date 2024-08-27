9ad8185002337335db79ceb4e503aa369f8c0bfe
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-27 16:18:28 +0200

    fix route css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './css/app.css'
+import './css/App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import Home from './components/Home'

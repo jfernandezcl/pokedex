2f97cb9e8e4d148ab5df421d7d57ef8643c0330e
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-28 21:59:44 +0200

    new API requests and styles

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -10,13 +10,18 @@ export default function Details({ pokemon }) {
     <div className="container-details">
       <div>
         <span className='details-name'>{letter(pokemon.name)}</span>
+        <span className='details-id'>{pokemon.id}</span>
       </div>
       <div>
         <img src={pokemon.sprites.other.dream_world.front_default}
           alt={pokemon.name} />
       </div>
       <div>
-
+        <span>Type: {pokemon.types.join(' / ')}</span>
+      </div>
+      <div>
+        <span>H {pokemon.height}</span>
+        <span>W {pokemon.weight}</span>
       </div>
     </div>
   )

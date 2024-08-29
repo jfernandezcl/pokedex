fa7c53abcf68703aad5cac989332dfa69712790f
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-29 13:00:18 +0200

    created new API request and classes

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -5,25 +5,35 @@ export default function Details({ pokemon }) {
   function letter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
   }
-
+  console.log(pokemon)
   return (
-    <div className="container-details">
-      <div>
-        <span className='details-name'>{letter(pokemon.name)}</span>
-        <span className='details-id'>{pokemon.id}</span>
+    <div>
+
+      <div className="container-pokemon">
+        <div>
+          <span className='pokemon-name'>{letter(pokemon.name)}</span>
+          <span className='pokemon-id'>{pokemon.id}</span>
+        </div>
+        <div>
+          <img className='pokemon-images' src={pokemon.image}
+            alt={pokemon.name} />
+        </div>
+        <div>
+          <span>Type: {pokemon.types.join(' / ')}</span>
+        </div>
+        <div className='pokemon-size'>
+          <span>Height {pokemon.height}</span>
+          <span>Weight {pokemon.weight}</span>
+        </div>
       </div>
-      <div>
-        <img className='details-images' src={pokemon.sprites.other.dream_world.front_default}
-          alt={pokemon.name} />
+
+      <div className='container-details'>
+        <span>Information: {pokemon.description}</span>
       </div>
-      <div>
-        <span>Type: {pokemon.types.join(' / ')}</span>
-      </div>
-      <div className='details-size'>
-        <span>Height {pokemon.height}</span>
-        <span>Weight {pokemon.weight}</span>
-      </div>
+
     </div>
+
+
 
   )
 
--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -8,7 +8,7 @@ import Details from "./Details";
 
 export default function PokemonDetail() {
   const { name } = useParams()
-  const [selectedPokemon, setSelectedPokemon] = useState()
+  const [selectedPokemon, setSelectedPokemon] = useState(null)
   const [error, setError] = useState(null)
 
   useEffect(() => {
@@ -19,9 +19,26 @@ export default function PokemonDetail() {
           throw new Error('Pokémon no encontrado')
         }
         const data = await response.json()
-        setSelectedPokemon(data)
+
+        const responseDescription = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`)
+        if (!responseDescription.ok) {
+          throw new Error('Pokémon no encontrado')
+        }
+        const dataDescription = await responseDescription.json()
+
+
+        const formattedData = {
+          id: data.id,
+          name: data.name,
+          height: data.height,
+          weight: data.weight,
+          types: data.types.map((typeInfo) => typeInfo.type.name),
+          image: data.sprites.other.home.front_default,
+          description: dataDescription.flavor_text_entries[0].flavor_text
+        }
+        setSelectedPokemon(formattedData)
         setError(null)
-      } catch {
+      } catch (error) {
         setSelectedPokemon(null)
         setError(error.message)
       }
@@ -42,17 +59,3 @@ export default function PokemonDetail() {
   )
 
 }
-
-
-
-/*if (loading) {
-  return (
-    <div className="container-spinner">
-      <div className="spinner"></div>
-    </div>
-  )
-}
-
-if (!pokemon) {
-  return <div>Pokémon not found</div>
-}*/

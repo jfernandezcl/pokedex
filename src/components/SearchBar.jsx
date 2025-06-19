e5bc9172ef93acc7ca84de03b39272688deb74cc
jfernandecl <javierfernandez0987@gmail.com>
2025-06-19 17:13:05 +0200

    refactor: improve code formatting and consistency in SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,18 @@
 import { useState } from "react";
-import '../css/SearchBar.css'
-import icon from '../images/iconsearch.png'
+import "../css/SearchBar.css";
+import icon from "../images/iconsearch.png";
 import { useNavigate } from "react-router-dom";
 
-
 export default function SearchBar({ onSearch }) {
-  const [searchTerm, setSearchTerm] = useState('')
-  const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
 
   const handleSearch = () => {
-    if (searchTerm.trim() !== '') {
-      navigate(`/pokemon/${searchTerm.toLowerCase()}`)
-      setSearchTerm('')
+    if (searchTerm.trim() !== "") {
+      navigate(`/pokemon/${searchTerm.toLowerCase()}`);
+      setSearchTerm("");
     }
-  }
+  };
 
   return (
     <div>
@@ -28,5 +27,5 @@ export default function SearchBar({ onSearch }) {
         <img src={icon} className="button-icon" alt="search-icon" />
       </button>
     </div>
-  )
+  );
 }

3d3f755984280d1a612be829ab0bf6617c899a49
jfernandecl <javierfernandez0987@gmail.com>
2025-06-19 17:11:40 +0200

    refactor: improve code formatting and structure in PokemonDetail component

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -2,30 +2,29 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Details from "./Details";
 
-
-
-
-
 export default function PokemonDetail() {
-  const { name } = useParams()
-  const [selectedPokemon, setSelectedPokemon] = useState(null)
-  const [error, setError] = useState(null)
+  const { name } = useParams();
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchPokemon = async () => {
       try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${name}`
+        );
         if (!response.ok) {
-          throw new Error('Pokémon no encontrado')
+          throw new Error("Pokémon no encontrado");
         }
-        const data = await response.json()
+        const data = await response.json();
 
-        const responseDescription = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`)
+        const responseDescription = await fetch(
+          `https://pokeapi.co/api/v2/pokemon-species/${name}`
+        );
         if (!responseDescription.ok) {
-          throw new Error('Pokémon no encontrado')
+          throw new Error("Pokémon no encontrado");
         }
-        const dataDescription = await responseDescription.json()
-
+        const dataDescription = await responseDescription.json();
 
         const formattedData = {
           id: data.id,
@@ -35,28 +34,32 @@ export default function PokemonDetail() {
           types: data.types.map((typeInfo) => typeInfo.type.name),
           image: data.sprites.other.home.front_default,
           images: data.sprites.other.home.front_shiny,
-          description: dataDescription.flavor_text_entries[0].flavor_text.replace(/\f/g, '')
-        }
-        setSelectedPokemon(formattedData)
-        setError(null)
+          description:
+            dataDescription.flavor_text_entries[0].flavor_text.replace(
+              /\f/g,
+              ""
+            ),
+        };
+        setSelectedPokemon(formattedData);
+        setError(null);
       } catch (error) {
-        setSelectedPokemon(null)
-        setError(error.message)
+        setSelectedPokemon(null);
+        setError(error.message);
       }
-    }
+    };
     if (name) {
-      fetchPokemon()
+      fetchPokemon();
     }
-
-  }, [name])
-
-
+  }, [name]);
 
   return (
     <div>
       {error && <p>{error}</p>}
-      {selectedPokemon ? <Details pokemon={selectedPokemon} /> : !error && <p>Loading...</p>}
+      {selectedPokemon ? (
+        <Details pokemon={selectedPokemon} />
+      ) : (
+        !error && <p>Loading...</p>
+      )}
     </div>
-  )
-
+  );
 }

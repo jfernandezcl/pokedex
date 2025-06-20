a9f035aa014b9b7cee65c760501c640a0e7a2ace
jfernandecl <javierfernandez0987@gmail.com>
2025-06-20 10:57:30 +0200

    refactor: standardize quotes and improve formatting in App and main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import './css/App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header'
-import Home from './components/Home'
-import About from './components/About'
-import PokemonDetail from './components/PokemonDetail';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import PokemonDetail from "./components/PokemonDetail";
 
 function App() {
-
   return (
     <>
       <Router>
@@ -15,11 +12,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path='/pokemon/:name' element={<PokemonDetail />} />
+          <Route path="/pokemon/:name" element={<PokemonDetail />} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

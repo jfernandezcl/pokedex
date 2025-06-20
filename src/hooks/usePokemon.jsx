9c1fe2414e329bfbb6a6dc7b60f5a406d53cba71
jfernandecl <javierfernandez0987@gmail.com>
2025-06-20 10:59:56 +0200

    refactor: remove unused imports and clean up code in usePokemon hook

--- src/hooks/usePokemon.jsx
+++ src/hooks/usePokemon.jsx
@@ -1,57 +1,63 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export const usePokemon = () => {
   const [pokemons, setPokemons] = useState([]);
   const [loading, setLoading] = useState(false);
   const [filteredPokemons, setFilteredPokemons] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1)
-  const [pokemonsPerPage] = useState(20) // Número de pokémon por página
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pokemonsPerPage] = useState(20);
 
   useEffect(() => {
     const fetchPokemons = async () => {
-      setLoading(true)
+      setLoading(true);
       const responses = await Promise.all(
         Array.from({ length: 150 }, (_, i) =>
           fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`)
         )
       );
 
-      // mapear y pedir la información
-      const data = await Promise.all(
-        responses.map((res) => res.json()) // convertir cada respuesta a json
-      );
+      const data = await Promise.all(responses.map((res) => res.json()));
       const formattedData = data.map((pokemon) => ({
         id: pokemon.id,
         name: pokemon.name,
         height: pokemon.height,
         weight: pokemon.weight,
         types: pokemon.types.map((typeInfo) => typeInfo.type.name),
-        image: pokemon.sprites.other.home.front_default // estructura-detalles del pokémon
-      }))
+        image: pokemon.sprites.other.home.front_default,
+      }));
       setPokemons(formattedData);
       setFilteredPokemons(formattedData);
-      setLoading(false)
-    }
+      setLoading(false);
+    };
 
     fetchPokemons();
   }, []);
 
-  //función de búsqueda o filtro para los pokémon
   const filterPokemons = (query) => {
     if (!query) {
-      setFilteredPokemons(pokemons) // si no hay consultas
+      setFilteredPokemons(pokemons);
     } else {
       const filtered = pokemons.filter((pokemon) =>
-        pokemon.name.toLowerCase().includes(query.toLowerCase()))
-      setFilteredPokemons(filtered)
+        pokemon.name.toLowerCase().includes(query.toLowerCase())
+      );
+      setFilteredPokemons(filtered);
     }
-  }
+  };
 
-  // Determinar lo Pokémon a mostrar en la página actual
-  const indexOfLastPokemon = currentPage * pokemonsPerPage
-  const indexOfFirstPokemon = indexOfLastPokemon - pokemonsPerPage
-  const currentPokemons = filteredPokemons.slice(indexOfFirstPokemon, indexOfLastPokemon)
+  const indexOfLastPokemon = currentPage * pokemonsPerPage;
+  const indexOfFirstPokemon = indexOfLastPokemon - pokemonsPerPage;
+  const currentPokemons = filteredPokemons.slice(
+    indexOfFirstPokemon,
+    indexOfLastPokemon
+  );
 
-  return { pokemons, currentPokemons, filterPokemons, currentPage, setCurrentPage, totalPages: Math.ceil(filteredPokemons.length / pokemonsPerPage), loading }
-}
+  return {
+    pokemons,
+    currentPokemons,
+    filterPokemons,
+    currentPage,
+    setCurrentPage,
+    totalPages: Math.ceil(filteredPokemons.length / pokemonsPerPage),
+    loading,
+  };
+};
